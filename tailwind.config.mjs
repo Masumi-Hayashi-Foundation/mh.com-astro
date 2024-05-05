8515fa3ad70f7f33b27fd18ddb0673e7c82995ad
dkeesey <460435+dkeesey@users.noreply.github.com>
2024-05-04 17:21:48 -0700

    fine-tune theme, cloudinary text overlays

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  plugins: [require("@tailwindcss/typography")],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontSize: {
-        base: "16px",
-        h1: "1.802rem",
-        h2: "1.602rem",
-        h3: "1.424rem",
-        h4: "1.266rem",
-        h5: "1.125rem",
-        h6: "1rem",
-        sm: "0.889rem",
-        tiny: "0.79rem",
-      },
       colors: {
         "museum-black": "#000",
         "museum-white": "#fff",
@@ -22,7 +12,9 @@ export default {
         "museum-beige": "#faf8f5",
         "museum-gold": "#bdaa8f", // Soft metallic gold for highlighting
       },
+      boxShadow: {
+        'upward': '0 -4px 10px -5px rgba(0, 0, 0, 0.3)',
+      }
     },
   },
-  plugins: [require("@tailwindcss/typography")],
 };
