7f839decda7be9cfb58884abc73f3da76b49dd73
dkeesey <460435+dkeesey@users.noreply.github.com>
2024-05-11 11:50:49 -0700

    single pages in collection now render

--- src/content/art-pages/internment-camps/config.ts
+++ src/content/art-pages/internment-camps/config.ts
@@ -1,7 +1,7 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
-const postsCollection = defineCollection({
+const artworksCollection = defineCollection({
   type: 'content',
   schema: z.object({
       cloudinaryId: z.string(),
@@ -22,23 +22,6 @@ const postsCollection = defineCollection({
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  posts: postsCollection,
+  artworks: artworksCollection,
 };
 
-/*
-content-type: artwork
-image: ./images/angler-pow-camp-guard-tower.jpg
-series: Japanese-Canadian Internment Camps
-slug: angler-pow-camp-guard-tower
-name: Angler Prisoner of War Camp, Guard Tower
-title: Angler Prisoner of War Camp, Guard Tower, Marathon, Ontario, Canada
-altTag: Picture of Angler Prisoner of War Camp, Guard Tower, Marathon, Ontario, Canada, by Dr. Masumi Hayashi
-media: Panoramic photo collage with Kodak Type-C prints
-year: 1997
-city: Marathon
-state: Ontario
-country: Canada
-size: 28in. x 32in.
-inventory: 1 framed, 1 unframed
-cloudinaryId: /internment-camps/angler-pow-camp-guard-tower_ogicae
-*/
